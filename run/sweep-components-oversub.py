1c5320affbc90c73b92b0cf17aa9a78d34b24cac
FaridZandi <faridzs76@gmail.com>
2025-02-25 08:36:50 -0500

    backup Tue 25 Feb 2025 08:36:50 AM EST

--- run/sweep-components-oversub.py
+++ run/sweep-components-oversub.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
             
             summary, results_dir = do_experiment(seed_range=seed_range, 
                                                  added_comparisons=comparisons,
-                                                 simulation_seed=777, 
+                                                 experiment_seed=777, 
                                                  **perm) 
             
             for summary_item in summary:    
