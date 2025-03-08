52dee1018163c8e3b1c2393a51dc21e05d574478
FaridZandi <faridzs76@gmail.com>
2025-03-08 00:48:34 -0500

    backup Sat 08 Mar 2025 12:48:34 AM EST

--- run/sweep-jobs-scale.py
+++ run/sweep-jobs-scale.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
             ("placement_mode", ["entropy"]), 
             ("ring_mode", ["letitbe"]), 
             ("desired_entropy", [0.5]),
-            ("oversub", [2]),
+            ("oversub", [1]),
             ("cmmcmp_range", [(0, 2)]), 
             ("fallback_threshold", [0.5]),
             ("comm_size", [(120 * m, 360 * m, 60 * m)]),
