8bbe46f0108baa4657e440498903d03200d73de4
FaridZandi <faridzs76@gmail.com>
2025-09-24 09:17:18 -0400

    backup Wed Sep 24 09:17:18 AM EDT 2025

--- run/thesis-test-4.py
+++ run/thesis-test-4.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
             ("job_sizes", [(8, 12)]),
             ("placement_mode", ["entropy"]), 
             ("ring_mode", ["letitbe"]), 
-            ("desired_entropy", [0.5]),
+            ("desired_entropy", [0.4]),
             ("oversub", [2]),
             ("cmmcmp_range", [(0, 2)]),
             ("fallback_threshold", [0.5]),
