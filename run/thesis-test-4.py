24b33b821a5d941acedaf12390127d1cd33f638f
FaridZandi <faridzs76@gmail.com>
2025-09-24 16:23:27 -0400

    backup Wed Sep 24 04:23:27 PM EDT 2025

--- run/thesis-test-4.py
+++ run/thesis-test-4.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
             ("machine_count", [480]),
             ("rack_size", [12]),
             # ("job_sizes", [(4, 16)]),
-            ("job_sizes", [(8, 48)]),
+            ("job_sizes", [(8, 96)]),
             ("placement_mode", ["entropy"]), 
             ("ring_mode", ["letitbe"]), 
             ("desired_entropy", [0.5]),
