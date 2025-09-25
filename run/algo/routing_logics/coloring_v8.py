07353a85869ce2166cab15a1ebe812c70927097a
FaridZandi <faridzs76@gmail.com>
2025-09-25 14:47:11 -0400

    backup Thu Sep 25 02:47:11 PM EDT 2025

--- run/algo/routing_logics/coloring_v8.py
+++ run/algo/routing_logics/coloring_v8.py
@@ -437,9 +437,13 @@ def route_flows_graph_coloring_v8(all_flows, rem, usage, num_spines,
     print("number of solution entries:", len(solutions), file=sys.stderr)
     
     if run_context["plot-merged-ranges"]:   
-        plot_path = "{}/routing/merged_ranges_{}.png".format(run_context["routings-dir"], suffix)  
-        merged_ranges = {k: [(r[0], r[1]) for r in v] for k, v in merged_ranges.items()}
-        plot_time_ranges(hash_to_time_ranges, dict(merged_ranges), 
+        plot_path = "{}/routing/merged_ranges_{}.png".format(run_context["routings-dir"], suffix) 
+        merged_ranges_for_plot = {} 
+        for key, ranges in merged_ranges.items():
+            flat_ranges = [(r[0], r[1]) for r in ranges]
+            time_range = [flat_ranges[0][0], flat_ranges[-1][1]]
+            merged_ranges_for_plot[key] = time_range
+        plot_time_ranges(hash_to_time_ranges, dict(merged_ranges_for_plot), 
                          needed_color_count, max_degrees, num_spines,
                          highlighted_ranges, None, plot_path, max_edge_count)
     
