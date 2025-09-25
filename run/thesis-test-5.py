91dc3a6cd9d899dd521c05475241e75429823659
FaridZandi <faridzs76@gmail.com>
2025-09-25 18:29:15 -0400

    backup Thu Sep 25 06:29:14 PM EDT 2025

--- run/thesis-test-5.py
+++ run/thesis-test-5.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
                                                  experiment_seed=777, 
                                                  worker_thread_count=50,
                                                  plot_stuff=False,
-                                                 throttle_search=False,
+                                                 throttle_search=True,
                                                  farid_rounds=10,
                                                  run_cassini_timing_in_subprocess=True, 
                                                  **perm) 
