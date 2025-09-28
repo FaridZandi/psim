3c30cfe8039640c09d0a9d162d4fa6a35278557f
FaridZandi <faridzs76@gmail.com>
2025-09-28 16:35:10 -0400

    backup Sun Sep 28 04:35:10 PM EDT 2025

--- run/thesis-test-8-small.py
+++ run/thesis-test-8-small.py
@@ -105,9 +105,10 @@ if __name__ == "__main__":
                                                  added_comparisons=comparisons,
                                                  experiment_seed=777, 
                                                  worker_thread_count=50,
-                                                 plot_stuff=False,
+                                                 plot_stuff=True,
                                                  throttle_search=True,
                                                  farid_rounds=100,
+                                                 placement_seeds=[6],
                                                  run_cassini_timing_in_subprocess=True, 
                                                  **perm) 
             
