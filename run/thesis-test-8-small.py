65ac504b7ab90ac1d8bbe5e0cf5d6d37f69036d3
FaridZandi <faridzs76@gmail.com>
2025-09-29 09:34:26 -0400

    backup Mon Sep 29 09:34:26 AM EDT 2025

--- run/thesis-test-8-small.py
+++ run/thesis-test-8-small.py
@@ -67,9 +67,9 @@ if __name__ == "__main__":
 
         exp_config = [
             ("sim_length", [400 * m]),
-            ("machine_count", [96]),
+            ("machine_count", [48]),
             ("rack_size", [8]),
-            ("job_sizes", [(16, 24)]),
+            ("job_sizes", [("10%", "25%")]),
             ("placement_mode", ["entropy"]), 
             ("ring_mode", ["letitbe"]), 
             ("desired_entropy", [0.6]),
