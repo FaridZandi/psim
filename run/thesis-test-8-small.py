67e6c79853fbc003f80ea51585430b8e7c37fbf1
FaridZandi <faridzs76@gmail.com>
2025-09-29 14:35:54 -0400

    backup Mon Sep 29 02:35:54 PM EDT 2025

--- run/thesis-test-8-small.py
+++ run/thesis-test-8-small.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     seed_range = 10
     m = 100
     
-    clean_up_sweep_files = True
+    clean_up_sweep_files = False
 
     original_exp_number = None
     if original_exp_number is not None: 
