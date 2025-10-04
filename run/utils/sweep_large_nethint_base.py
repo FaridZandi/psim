b434feb73543e08f5177b2f2ce0ffe9fe9e4521e
FaridZandi <faridzs76@gmail.com>
2025-10-03 23:51:51 -0400

    backup Fri Oct  3 11:51:51 PM EDT 2025

--- run/utils/sweep_large_nethint_base.py
+++ run/utils/sweep_large_nethint_base.py
@@ -200,6 +200,7 @@ def calc_timing(timing_file_path, routing_file_path, placement_seed,
         env = os.environ.copy()
         env["PYTHONHASHSEED"] = "12345"  # any fixed int as a string (0..4294967295)
 
+        config_sweeper_run_scheduler = None 
         if config_sweeper_run_scheduler is not None:
             exe = [str(config_sweeper_run_scheduler)]
         else:
@@ -224,6 +225,8 @@ def calc_timing(timing_file_path, routing_file_path, placement_seed,
             print("subprocess error output: ", err_output)
             print("subprocess standard output: ", stdout.decode("utf-8"))
             
+            input("Press Enter to continue..."  
+                  )
             with open(run_context["output-file"], "a") as f:
                 f.write(err_output) 
                 
